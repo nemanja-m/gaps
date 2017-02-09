bb2a3c37dda18daa6c97a46e4e0d81b364c9eec5
nemanja-m <nemanja_milicevic@hotmail.com>
2017-02-09 23:08:17 +0100

    Represent puzzle as array of pieces

--- solver/individual.py
+++ solver/individual.py
@@ -6,19 +6,29 @@ class Individual:
     Individual object is one of the solutions to the problem (possible arrangement of the puzzle's pieces).
     It is created by random shuffling initial puzzle.
 
-    :param puzzle: 2D matrix representing initial puzzle.
+    :param pieces:      Array of pieces representing initial puzzle.
+    :param rows:        Number of rows in input puzzle
+    :param columns:     Number of columns in input puzzle
 
     Usage::
 
         >>> from individual import Individual
-        >>> ind = Individual(puzzle)
+        >>> ind = Individual(pieces, 10, 15)
 
     """
 
-    def __init__(self, puzzle):
-        permuted_puzzle = np.random.permutation(puzzle)
+    def __init__(self, pieces, rows, columns):
+        self.pieces     = pieces
+        self.rows       = rows
+        self.columns    = columns
 
-        for row in permuted_puzzle:
-            np.random.shuffle(row)
+        np.random.shuffle(self.pieces)
 
-        self.puzzle = permuted_puzzle
+    def __getitem__(self, key):
+        return self.pieces[key * self.columns : (key + 1) * self.columns]
+
+    def piece_size(self):
+        if len(self.pieces) > 0:
+            return self.pieces[0].shape[0]
+        else:
+            return -1
