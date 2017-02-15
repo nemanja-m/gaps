86ab463887e6c8a4a5de588aaa348f7d4aea50b7
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-02-15 04:59:38 +0100

    Log evolution progress

--- solver/algorithm.py
+++ solver/algorithm.py
@@ -2,19 +2,45 @@ from solver import helpers
 from solver import fitness
 from solver.operators  import select
 from solver.operators  import crossover
-from solver.individual import Individual
+from solver.models import Individual
+from solver.cache import DissimilarityMeasureCache
+from operator import attrgetter
+import time
 
-def create_population(problem, population_size):
-    return [Individual(problem) for i in range(population_size)]
+def best_individual(population):
+    """Returns the fittest individual from population"""
+    return min(population, key=attrgetter("fitness"))
 
-def start_evolution(problem, population_size=1000, generations=100):
-    population = create_population(population_size)
+def start_evolution(image, piece_size=28, population_size=1000, generations=5, verbose=True):
+    starting_time       = time.time()
+    total_running_time  = 0
+    time_per_generation = []
+
+    # Create population
+    pieces, rows, columns = helpers.flatten_image(image, piece_size, indexed=True)
+    population = [Individual(pieces, rows, columns) for i in range(population_size)]
+
+    if verbose:
+        print "[INFO] Created population in {:.2f} s".format(time.time() - starting_time)
+        print "[INFO] Starting evolution with {} generations ...".format(generations)
+        print "\n|{:^12} | {:^12} | {:^10} | {:^12}|".format("Generation", "Duration", "Hit Rato", "Miss Ratio")
+        print "+-------------+--------------+------------+-------------+"
 
     for generation in range(generations):
         new_population = []
 
+        start = time.time()
+
         # Parallelize this !
         map(fitness.evaluate, population)
+
+        if verbose:
+            print "|{:^12} | {:>10.2f} s | {:>8} % | {:>10} %|".format(generation,
+                                                                       time.time() - start,
+                                                                       DissimilarityMeasureCache.hit_ratio(),
+                                                                       DissimilarityMeasureCache.miss_ratio())
+
+        DissimilarityMeasureCache.reset_stats()
 
         while len(new_population) <= population_size:
             first_parent  = select(population)
@@ -24,6 +50,10 @@ def start_evolution(problem, population_size=1000, generations=100):
             new_population.append(child)
 
         population = new_population
+
+    if verbose:
+        print "+-------------+--------------+------------+-------------+"
+        print "\n[INFO] Total running time {:.2f} s".format(time.time() - starting_time)
 
     return best_individual(population)
 
