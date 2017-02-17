ebdce2d156b49d2254f69d5abc33eb1dc7a0b5a9
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-02-17 19:02:17 +0100

    Normalize color difference

--- solver/fitness.py
+++ solver/fitness.py
@@ -21,7 +21,7 @@ def evaluate(individual):
     # For each two adjancent pieces in rows
     for i in range(individual.rows):
         for j in range(individual.columns - 1):
-            fitness_value += dissimilarity_measure(individual[i][j], individual[i][j + 1])
+            fitness_value += dissimilarity_measure(individual[i][j], individual[i][j + 1], orientation="LR")
 
     # For each two adjancent pieces in columns
     for i in range(individual.rows - 1):
@@ -63,15 +63,15 @@ def dissimilarity_measure(first_piece, second_piece, orientation="LR"):
 
     # | L | - | R |
     if orientation == "LR":
-        color_difference = first_piece[0:rows, columns - 1, 0:3] / 255 - second_piece[0:rows, 0, 0:3] / 255
+        color_difference = first_piece[0:rows, columns - 1, 0:3] - second_piece[0:rows, 0, 0:3]
 
     # | T |
     #   |
     # | D |
     if orientation == "TD":
-        color_difference = first_piece[rows - 1, 0:columns, 0:3] / 255 - second_piece[0, 0:columns, 0:3] / 255
+        color_difference = first_piece[rows - 1, 0:columns, 0:3] - second_piece[0, 0:columns, 0:3]
 
-    squared_color_difference = np.power(color_difference, 2)
+    squared_color_difference = np.power(color_difference / 255.0, 2)
     color_difference_per_row = np.sum(squared_color_difference, axis=1)
     total_difference         = np.sum(color_difference_per_row, axis=0)
 
