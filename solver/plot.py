83eb948e4ad1de36a8f0082f1e0b85146cc72107
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-09-06 02:50:55 -0500

    Ignore matplotlib deprecation warning

--- solver/plot.py
+++ solver/plot.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
+import warnings
+import matplotlib.cbook
+
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 
 
 class Plot(object):
 
     def __init__(self, image, title="Initial problem"):
         # Let image fill the figure
-        fig = plt.figure(frameon=False)
+        fig = plt.figure(figsize=(9, 7), frameon=False)
         ax = plt.Axes(fig, [0., 0., 1., .9])
         ax.set_axis_off()
         fig.add_axes(ax)
@@ -18,5 +22,5 @@ class Plot(object):
         self._current_image.set_data(image)
         plt.draw()
 
-        # Give pyplot 0.1s to draw image
-        plt.pause(0.1)
+        # Give pyplot 0.05 to draw image
+        plt.pause(0.05)
