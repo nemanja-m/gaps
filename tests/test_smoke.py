03dc310789089916d5eff8365f8a1bebcfb52d16
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-09-11 18:58:52 -0500

    Add smoke test

---
+++ tests/test_smoke.py
@@ -0,0 +1,24 @@
+import pytest
+import numpy as np
+
+from scipy import misc
+from gaps import image_helpers
+from gaps.genetic_algorithm import GeneticAlgorithm
+
+GENERATIONS = 3
+POPULATION = 100
+PIECE_SIZE = 128
+
+image = misc.imread("images/baboon.jpg")
+
+@pytest.fixture
+def puzzle():
+    pieces, rows, columns = image_helpers.flatten_image(image, PIECE_SIZE)
+    np.random.shuffle(pieces)
+    return image_helpers.assemble_image(pieces, rows, columns)
+
+def test_puzzle_solver(puzzle):
+    algorithm = GeneticAlgorithm(puzzle, PIECE_SIZE, POPULATION, GENERATIONS)
+    solution = algorithm.start_evolution(verbose=False)
+
+    assert np.array_equal(image, solution.to_image())
